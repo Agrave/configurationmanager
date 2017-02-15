8711f99e960f538d974f4c9d831393ec7a27ccb2
Artem Mogilnikov <artem.mogilnikov@gmail.com>
2017-02-15 23:21:07 +0200

    refactor return WebDriver

--- src/main/java/TestBrowserManager/factory/CloudTestBrowser.java
+++ src/main/java/TestBrowserManager/factory/CloudTestBrowser.java
@@ -3,26 +3,21 @@ package TestBrowserManager.factory;
 import ConfigurationManager.ConfigurationManager;
 import TestBrowserManager.api.BrowserType;
 import TestBrowserManager.api.TestBrowserFactory;
+import org.openqa.selenium.WebDriver;
 
 
 public class CloudTestBrowser implements TestBrowserFactory {
     @Override
-    public String create() {
+    public WebDriver create() {
         String browserName = ConfigurationManager.getInstance().getTestBrowser().toUpperCase();
         BrowserType browserType=BrowserType.valueOf(browserName);
         switch (browserType) {
             case CHROME:
-                return "Cloud Google Chrome";
             case FIREFOX:
-                return "Cloud Firefox";
             case EDGE:
-                return "Cloud Edge";
             case IE:
-                return "Cloud IE";
             case OPERA:
-                return "Cloud Opera";
             case SAFARI:
-                return "Cloud Safari";
             default:
                 return null;
         }
